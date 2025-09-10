2b538cb50cd4fa9b0fb780c15d1dc51f77aa1e5d
Thanmayi-14 <rthanmayi_cse2305b0@mgit.ac.in>
2025-09-11 01:36:31 +0530

    Create mul.py

---
+++ mul.py
@@ -0,0 +1,11 @@
+# Multiplication of two numbers
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Multiplying
+result = num1 * num2
+
+# Displaying result
+print(f"The multiplication of {num1} and {num2} is {result}")
